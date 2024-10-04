eedea3b1bedbc70fab1cdd506749b08ddaa776d2
yashVSawant <yashsawant23910@gmail.com>
2024-10-04 16:26:38 +0530

    error handler added

--- src/App.js
+++ src/App.js
@@ -2,26 +2,46 @@ import React,{useState} from 'react';
 
 import MoviesList from './components/MoviesList';
 import Loader from './components/Loader';
+import { Button } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   const [movies , setMovies] = useState([]);
-  const [isLoading , setIsLoading] = useState(false)
+  const [isLoading , setIsLoading] = useState(false);
+  const [error ,setError] = useState(null);
+  const [isRetry , setIsRetry] = useState(true);
+
   const fetchMoveHandler = async()=>{
-    setIsLoading(true)
+    setMovies([])
+    setIsLoading(true);
+    setError(null);
+
     try {
-      const filmsApiResponse = await fetch('https://swapi.dev/api/films/');
-      const fetchedMovies = await filmsApiResponse.json()
+      const filmsApiResponse = await fetch('https://swapi.dev/api/film');
+      if(!filmsApiResponse.ok)throw new Error('Something went wrong!...Retrying');
+      const fetchedMovies = await filmsApiResponse.json();
       const movies = fetchedMovies.results.map(movie=>{return{
         id:movie.episode_id,title:movie.title , releaseDate:movie.release_date ,openingText:movie.opening_crawl
       }});
       setMovies(movies)
       setIsLoading(false)
     } catch (err) {
-      console.log(err)
-      isLoading(false)
+      setError(err.message);
+      setIsLoading(false);
+      if(isRetry)retry()
     }
   }
+  const retry = ()=>{
+    setTimeout(() => {
+      fetchMoveHandler()
+    }, 5000);
+  }
+
+  const cancelRetryHandler = ()=>{
+    setIsRetry(false);
+    console.log(isRetry)
+  }
+
 
   return (
     <React.Fragment>
@@ -29,8 +49,9 @@ function App() {
         <button onClick={fetchMoveHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList isLoading={isLoading} movies={movies} />}
-        {!isLoading && <p>movies not fetched yet!</p>}
+        {!isLoading && movies.length > 0 && <MoviesList isLoading={isLoading} movies={movies} />}
+        {!isLoading && movies.length ===0 && !error && <p>movies not fetched yet!</p>}
+        {!isLoading && error && <p>{error} <Button onClick={cancelRetryHandler}>Cancel Retry</Button></p>}
         {isLoading && <Loader/>}
       </section>
     </React.Fragment>
